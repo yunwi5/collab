a0293c021ad2fa477867a0266b29a69f5b1aae5d
yunwi5 <1009jyk@gmail.com>
2023-01-08 06:02:29 +1300

    Update s3 tf

--- server/terraform/s3.tf
+++ server/terraform/s3.tf
@@ -12,7 +12,42 @@ resource "aws_s3_bucket" "image_s3" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "image_s3_acl" {
   bucket = aws_s3_bucket.image_s3.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_public" {
+  bucket = aws_s3_bucket.image_s3.id
+  policy = data.aws_iam_policy_document.allow_access_from_public_document.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_public_document" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.image_s3.arn,
+      "${aws_s3_bucket.image_s3.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "image_s3_cors" {
+  bucket = aws_s3_bucket.image_s3.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD", "PUT"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
 }
