5bf55f2b71d007b654c5bbf000461abce458d8cf
yunwi5 <1009jyk@gmail.com>
2023-01-28 23:58:52 +1300

    Add auth test util

---
+++ server/test/auth/auth.util.ts
@@ -0,0 +1,66 @@
+import request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import {
+  SIGN_UP_MUTATION,
+  SIGN_UP_OPERATION_NAME,
+  generateSignUpVariables,
+} from './signup.helper';
+import { User } from 'src/users/entities';
+import { SIGN_IN_MUTATION, SIGN_IN_OPERATION_NAME } from './signin.helper';
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+type SignUpInput = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export const signUp = async (
+  app: INestApplication,
+  signUpInput: SignUpInput,
+) => {
+  let user: User;
+
+  await request(app.getHttpServer())
+    .post(GRAPHQL_ENDPOINT)
+    .send({
+      operationName: SIGN_UP_OPERATION_NAME,
+      query: SIGN_UP_MUTATION,
+      variables: { signUpInput },
+    })
+    .expect(res => {
+      user = res.body.data.signUp;
+    });
+
+  return user;
+};
+
+export const signUpAndIn = async (app: INestApplication) => {
+  const signUpInput = generateSignUpVariables().signUpInput;
+  let authResponse: {access_token: string, user: User};
+
+  await signUp(app, signUpInput);
+
+  await request(app.getHttpServer())
+    .post(GRAPHQL_ENDPOINT)
+    .send({
+      operationName: SIGN_IN_OPERATION_NAME,
+      query: SIGN_IN_MUTATION,
+      variables: {
+        signInInput: {
+          signInInput: {
+            username: signUpInput.username,
+            password: signUpInput.password,
+          },
+        },
+      },
+    })
+    .expect(200)
+    .expect(res => {
+        const { access_token, user } = res.body.data.signIn;
+        authResponse = {access_token, user};
+    });
+
+    return authResponse;
+};
